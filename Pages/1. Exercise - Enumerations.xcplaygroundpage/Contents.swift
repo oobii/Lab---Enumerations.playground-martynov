afd522f18ef703596755b5e3c803c6e8c30be792
martynov <vvayfarer@yandex.com>
2017-11-13 17:28:26 -0500

    first commit

---
+++ Pages/1. Exercise - Enumerations.xcplaygroundpage/Contents.swift
@@ -0,0 +1,70 @@
+/*:
+ ## Exercise - Enumerations
+ 
+ Define a `Suit` enum with four possible cases: `clubs`, `spades`, `diamonds`, and `hearts`.
+ */
+enum Suit {
+    case clubs
+    case spades
+    case diamonds
+    case hearts
+}
+
+var cardInHand = Suit.hearts
+print("This ia card in hand: \(cardInHand)")
+/*:
+ Imagine you are being shown a card trick and have to draw a card and remember the suit. Create a variable instance of `Suit` called `cardInHand` and assign it to the `hearts` case. Print out the instance.
+ */
+
+/*:
+ Now imagine you have to put back the card you drew and draw a different card. Update the variable to be a spade instead of a heart.
+ */
+cardInHand = .spades
+
+
+/*:
+ Imagine you are writing an app that will display a fun fortune (i.e. something like "You will soon find what you seek.") based on cards drawn. Write a function called `getFortune(cardSuit:)` that takes a parameter of type `Suit`. Inside the body of the function, write a switch statement based on the value of `cardSuit`. Print a different fortune for each `Suit` value. Call the function a few times, passing in different values for `cardSuit` each time.
+ */
+func getFortune(suit: Suit) {
+    
+    switch suit {
+        case .clubs :
+            print("You get what you want")
+        case .spades :
+            print("You will get millions of dollars!")
+        case .diamonds:
+            print("You are strong amd persistent as a diamond!")
+        case .hearts :
+            print("You love and loved!")
+        
+    }
+}
+
+getFortune(suit: .clubs)
+getFortune(suit: .spades)
+getFortune(suit: .hearts)
+/*:
+ Create a `Card` struct below. It should have two properties, one for `suit` of type `Suit` and another for `value` of type `Int`.
+ */
+struct Card {
+    
+    enum Value {
+        case ace, two, three, four, five , six, seven, eight,nine,ten, jack, queen, king
+    }
+    
+    var suit: Suit
+    var value: Value
+}
+
+
+
+/*:
+ How many values can playing cards have? How many values can `Int` be? It would be safer to have an enum for the card's value as well. Inside the struct above, create an enum for `Value`. It should have cases for `ace`, `two`, `three`, `four`, `five`, `six`, `seven`, `eight`, `nine`, `ten`, `jack`, `queen`, `king`. Change the type of `value` from `Int` to `Value`. Initialize two `Card` objects and print a statement for each that details the card's value and suit.
+ */
+var cardOne = Card(suit: .spades, value: .ace)
+var cardTwo = Card(suit: .hearts, value: .queen)
+
+print("These are the cards: \(cardOne.value) of \(cardOne.suit) and \(cardTwo.value) of \(cardTwo.suit)")
+
+
+//: page 1 of 2  |  [Next: App Exercise - Swimming Workouts](@next)
